fda281dd5499ba2d3995c50ea31293ce7c6ce99c
Nad GarDez <iranad@hotmail.com>
2025-12-14 08:39:16 -0400

    feat: finished login config

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express, { Application, Request, Response } from 'express';
+var cors = require('cors');
 
 const app: Application = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, TypeScript + Express!');
 });
 
 app.listen(PORT, () => {
-       console.log(`Server running at http://localhost:${PORT}`);
-});
